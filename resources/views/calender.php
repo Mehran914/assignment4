de38ee6bc2f36943e9cfc36e8b86c18473ec17ff
Mehran914 <mehran914@hotmail.com>
2015-08-19 19:42:43 +0500

    laravel

---
+++ resources/views/calender.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calender Page</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+</head><body>
+
+<iframe src="https://www.google.com/calendar/embed?src=12bscsmali%40seecs.edu.pk&ctz=Asia/Karachi" style="border: 0" width="800" height="600"
+</body>
+
+</html>
